a0065cd388980c60584dd8c24ba7b2daacf9f892
yeonseok <leeys4140@naver.com>
2024-08-01 17:13:07 +0900

    selection sort 예시

---
+++ my-scripts/chapter_6/6-1.py
@@ -0,0 +1,23 @@
+# array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+#
+# for i in range(len(array)):
+#     min_value, min_index = 99999, 0
+#     for j in range(i + 1, len(array)):
+#         if min_value >= array[j]:
+#             min_value, min_index = array[j], j
+#     array[i], array[min_index] = min_value, array[i]
+#
+# print(array)
+
+
+# ---------------------book answer ------------------------
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(len(array)):
+    min_index = i  # 가장 작은 원소의 인덱스
+    for j in range(i + 1, len(array)):
+        if array[min_index] > array[j]:
+            min_index = j
+    array[i], array[min_index] = array[min_index], array[i]  # 스와프
+
+print(array)
